2676c661c7569d4aa2f73bbade66506e73efb6d4
fitzy1293 <omfitzgerald61298@gmail.com>
2020-12-05 06:10:55 +0000

    Berkshire County COVID update: 12/03/2020

--- covid-update.sh
+++ covid-update.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 python3 nytimes_covid.py -getdata
-commitMessage="$(grep 'Most Recent Update:' head.md | sed -e 's/\*//g' -e 's/*: //')"
+commitMessage="$(grep 'Most Recent Update:' head.md | sed -e 's/\*//g' -e 's/Most Recent Update: //')"
 echo "$commitMessage"
 
 git add .
