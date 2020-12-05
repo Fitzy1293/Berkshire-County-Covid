cc1c5fac15660c81627a3b66213816254edd0977
fitzy1293 <omfitzgerald61298@gmail.com>
2020-12-05 23:57:54 +0000

    Berkshire County COVID update: Sat Dec  5 23:57:54 UTC 2020

--- covid-update.sh
+++ covid-update.sh
@@ -1,13 +1,20 @@
-#!/bin/bash
+#!/bin/env bash
+
 function run {
-        python3 nytimes_covid.py
-        commitMessage="$(grep 'Most Recent Update:' head.md | sed -e 's/\*//g' -e 's/Most Recent Update: //')"
-        git add .
-        git commit -m "Berkshire County COVID update: $commitMessage"
-        git push -u origin "$(git status | head -n 1 | sed 's/.* //')"
-}
-while true; do
     date > run_date
-    run
-    sleep 1s
+    python3 nytimes_covid.py
+
+    git add .  &&
+    git commit -m "Berkshire County COVID update: $(date)" &&
+    git push origin "$(git status | head -n 1 | sed 's/.* //')"
+
+
+}
+run &&
+while true; do
+
+run
+sleep 5m
+
+
 done
