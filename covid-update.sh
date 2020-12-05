68f7f3e56780f50dca298c52fa800ff3c4bd5716
fitzy1293 <omfitzgerald61298@gmail.com>
2020-12-04 23:33:18 -0500

    Berkshire County COVID update:

---
+++ covid-update.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+commitMessage="$(date +'%m/%d/%Y' | sed 's#/#-#g')"
+
+git add .
+git commit -m "Berkshire County COVID update: ${commit}"
+git push -u origin master
+
+echo "$commitMessage"
