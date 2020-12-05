5eeec4c2728d1b0d2aa632ad1aa9a3488e1927e6
Ubuntu <ubuntu@ip-172-26-13-82.us-east-2.compute.internal>
2020-12-05 05:55:35 +0000

    Berkshire County COVID update: 12-05-2020

--- covid-update.sh
+++ covid-update.sh
@@ -3,5 +3,6 @@
 commitMessage="$(date +'%m/%d/%Y' | sed 's#/#-#g')"
 
 git add .
-git commit -m "Berkshire County COVID update: ${commit}"
+git commit -m "Berkshire County COVID update: $commitMessage"
 git push -u origin master
+echo "Commit message: $commitMessage"
