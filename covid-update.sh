8c042f1f361baf9c1b8d27a602a2b8a946101e78
fitzy1293 <omfitzgerald61298@gmail.com>
2020-12-05 22:52:14 -0500

    Auto update change

--- covid-update.sh
+++ covid-update.sh
@@ -4,13 +4,12 @@ function run {
     date > run_date
     python3 nytimes_covid.py -getdata
 
-    git add .  &&
+    git add COVID_plots.png All_Berkshire_Data_Provided.csv README.md &&
     git commit -m "Berkshire County COVID update: $(date)" &&
     git push origin "$(git status | head -n 1 | sed 's/.* //')"
-
-
 }
-run &&  
+
+run &&
 while true; do
 
 run
