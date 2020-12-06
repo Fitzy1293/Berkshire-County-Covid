430abda6ed3cd1b8421ed2b976f9b3e5a71eb6a7
fitzy1293 <omfitzgerald61298@gmail.com>
2020-12-05 22:40:13 -0500

    Various .py organization changes

--- nytimes_covid.py
+++ nytimes_covid.py
@@ -3,10 +3,9 @@
 from pprint import pprint
 import os
 import sys
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 
+
+import covid_plot
 
 def berkshireRows(file):  # Parses CSVs
     head = ['date','county','state','fips','cases','deaths']
@@ -16,67 +15,22 @@ def berkshireRows(file):  # Parses CSVs
 def dateFormat(nytDate):
     return '/'.join(nytDate.split('-')[1:] + [nytDate.split('-')[0]])
 
-# Gets the deltas between days for cases and deaths.
-def shortenTable(rows):
-    fixedFirstDay = [
-                dateFormat(rows[1][0]),
-                f'{rows[1][-2]} Δ={rows[1][-2]}',
-                f'{rows[1][-1]} Δ={rows[1][-1]}'
-                ]
+def createNewRow(row, previous):
+    cases = f'{row[-2]}: Δ={int(row[-2]) - int(previous[-2])}'
+    deaths = f'{row[-1]}: Δ={int(row[-1]) - int(previous[-1])}'
+    return [row[0], cases, deaths, row[1:]]
 
+def shortenTable(rows):
+    previous = rows[1][1:]
+    fixedFirstDay = [dateFormat(previous[0]), f'{previous[-2]} Δ={previous[-2]}', f'{previous[-1]} Δ={previous[-1]}']
     returnRows = [fixedFirstDay]
 
-    previous = rows[1]
     for i in rows[2:]:
-        row = [dateFormat(i[0])]
-        caseDiff = f'{i[-2]}: Δ={int(i[-2]) - int(previous[-2])}'
-        deathDiff = f'{i[-1]}: Δ={int(i[-1]) - int(previous[-1])}'
-
-        row.extend((caseDiff, deathDiff))
-        returnRows.append(row)
-        previous = i
+        newRow = createNewRow(i, previous)
+        returnRows.append(newRow[0:-1])
+        previous = newRow[-1]
 
     return returnRows
-
-def plotCovid(rows):
-    cases = [int(i[-2]) for i in rows[1:]]
-    deaths = [int(i[-1]) for i in rows[1:]]
-
-    startDate = dateFormat(rows[1][0])
-    endDate = dateFormat(rows[-1][0])
-
-    gs = gridspec.GridSpec(2, 2) # Create 2x2 sub plots
-
-    fig = plt.figure()
-
-    ax1 = fig.add_subplot(gs[0, 1]) # row 0, col 0
-    ax1.plot(cases, 'r.-')
-    ax1.set(xlabel=f'Days since {startDate}', ylabel='Cases',
-           title='Berkshire County, MA - COVID-19 cases')
-
-    ax2 = fig.add_subplot(gs[1, 1]) # row 0, col 1
-    ax2.plot(deaths, 'm.')
-    ax2.set(xlabel=f'Days since {startDate}', ylabel='Total deaths',
-           title='Berkshire County, MA - COVID-19 deaths')
-    ax2.plot([0,1])
-
-    ax3 = fig.add_subplot(gs[:, 0]) # col 1, span all rows
-    ax3.plot(cases, 'r')
-    ax3.plot(deaths, 'm')
-    plt.fill_between(np.arange(0, len(cases)), deaths, cases,
-                 facecolor="orange", # The fill color
-                 color='r',       # The outline color
-                 alpha=0.2)
-    plt.fill_between(np.arange(0, len(cases)), deaths,
-                 facecolor="orange", # The fill color
-                 color='m',       # The outline color
-                 alpha=0.2)
-
-    ax3.set(xlabel=f'Days since {startDate}', ylabel='COVID-19 cases and deaths',
-           title=f'COVID-19 Tracking\nBerkshire County, MA\n{startDate} - {endDate}')
-
-    fig.set_size_inches(15, 15)
-    plt.savefig("COVID_plots.png", dpi = 100)
 
 def csvCreate(rows):
     with open('All_Berkshire_Data_Provided.csv', 'w+') as f:
@@ -105,12 +59,13 @@ if __name__ == '__main__':
         print()
 
     rows = berkshireRows(curledCsv)
-    plotCovid(rows)
+    covid_plot.plotCovid(rows)
 
-    finalRows = [['Date', 'Cases (Total Δ=Daily Change)', 'Deaths (Total Δ=Daily Change)']] + [i for i in reversed(shortenTable(rows))]
-    csvCreate(finalRows)
+    outputTable = [i for i in reversed(shortenTable(rows))]
+    head = ['Date', 'Cases (Total Δ=Daily Change)', 'Deaths (Total Δ=Daily Change)']
+    csvCreate([head] + outputTable)
+
     os.system('csvtomd All_Berkshire_Data_Provided.csv > markdown_table.md')
-
 
     nytDate = dateFormat(rows[-1][0])
 
@@ -119,4 +74,3 @@ if __name__ == '__main__':
     os.system('cp README.md previous_README.md')
     updateReadme(nytDate)
     print('README.md is up to date')
-
