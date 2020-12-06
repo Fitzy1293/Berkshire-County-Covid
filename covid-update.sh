9ba5aff1241c6bdf01c306411db4680e2731881e
fitzy1293 <omfitzgerald61298@gmail.com>
2020-12-05 23:26:43 -0500

    proper run_update logging

--- covid-update.sh
+++ covid-update.sh
@@ -1,20 +1,18 @@
 #!/bin/env bash
 
 function run {
-    date > run_date
+
     python3 nytimes_covid.py -getdata
 
-    git add COVID_plots.png All_Berkshire_Data_Provided.csv README.md &&
+    git add COVID_plots.png All_Berkshire_Data_Provided.csv README.md run_date &&
     git commit -m "Berkshire County COVID update: $(date)" &&
     git push origin "$(git status | head -n 1 | sed 's/.* //')"
 }
 
-date > run_date
-run &&
+
 while true; do
 
-run
-sleep 6h
+date > run_date && run && sleep 6h
 
 
 done
