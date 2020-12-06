6a3eb3cc1286c7272004c7fb6c9355a080d6b0c3
fitzy1293 <omfitzgerald61298@gmail.com>
2020-12-06 00:32:12 +0000

    Berkshire County COVID update: Sun Dec  6 00:32:12 UTC 2020

--- covid-update.sh
+++ covid-update.sh
@@ -10,7 +10,7 @@ function run {
 
 
 }
-run  
+run &&  
 while true; do
 
 run
--- repo-update.sh
+++ repo-update.sh
@@ -1,2 +1,2 @@
 #!/bin/env bash
-exec nohup ./covid-update.sh > /dev/null 2>&1
+nohup ./covid-update.sh > /dev/null 2>&1 &
